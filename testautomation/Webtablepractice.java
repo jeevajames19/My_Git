a09f1440bea6ec26113e18b8599f50f4bfa5f966
jeevajames19 <jeevatommy2000@gmail.com>
2023-02-07 22:00:28 +0530

    git project

---
+++ testautomation/Webtablepractice.java
@@ -0,0 +1,21 @@
+package testautomation;
+
+import org.openqa.selenium.WebDriver;
+
+public class Webtablepractice {
+WebDriver driver ;
+	public void webtable()
+	{
+	
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
